9d6f61ce16a796eaf09f6f02a198095cb75a5ff5
Brendson <brendson.net@gmail.com>
2022-06-17 22:38:48 -0300

    feat: created user getId route

--- src/main/java/com/br/server/controller/UserController.java
+++ src/main/java/com/br/server/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.br.server.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,10 @@ public class UserController {
   @GetMapping("/user")
   public List<User> read(){
     return this.userRepository.findAll();
+  }
+
+  @GetMapping("/user/{id}")
+  public Optional<User> readId(@PathVariable(value = "id") Long id){
+    return this.userRepository.findById(id);
   }
 }
